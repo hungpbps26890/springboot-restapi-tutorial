4d7c6cf1d9df0f50e141fb3c9e6c15f3f64f2495
hungpbps26890 <hungpbps26890@fpt.edu.vn>
2025-02-18 10:09:07 +0700

    Configure Datasource in application.yml and specify Customer class as an Entity

--- src/main/java/com/dev/demo/customer/Customer.java
+++ src/main/java/com/dev/demo/customer/Customer.java
@@ -1,5 +1,6 @@
 package com.dev.demo.customer;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 @NoArgsConstructor
@@ -7,7 +8,11 @@ import lombok.*;
 @Getter
 @Setter
 @Builder
+@Entity
+@Table(name = "customers")
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
